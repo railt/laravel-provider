297812a6f417daada8ac97e8330062ba0496cd30
Serafim <nesk@xakep.ru>
2017-08-31 04:44:52 +0300

    Add base test case

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of Railt Laravel Adapter package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Railt\Adapters\Laravel\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTestCase
+ * @package Railt\Adapters\Laravel\Tests
+ */
+abstract class AbstractTestCase extends TestCase
+{
+}
